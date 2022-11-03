14d79a20bf9fdad3eb7b9362ddf10751f0f3777f
zhuwenfu <2583243657@qq.com>
2022-11-03 17:04:05 +0800

    feat:swagger集成

--- invoicing-backend/src/main.ts
+++ invoicing-backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import {DocumentBuilder,SwaggerModule} from '@nestjs/swagger'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.useGlobalFilters(new HttpExceptionFilter())
+  //设置swagger文档
+  const swaggerOptions = new DocumentBuilder()
+  .setTitle('进销存信息系统后端api可视化')
+  .setDescription('PowerBy NestJs&Swagger.\nDesignBy Zhuwenfu')
+  .build();
+  const document = SwaggerModule.createDocument(app,swaggerOptions);
+  SwaggerModule.setup('doc',app,document)
   await app.listen(3000);
 }
 bootstrap();
